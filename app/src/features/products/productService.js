0d65273039126c4b8b4bf6e9b0a98e73ab883975
Aaron Wagner <aaron@aaronwagner.net>
2022-08-16 19:18:56 -0700

    whoops

--- app/src/features/products/productService.js
+++ app/src/features/products/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://137.184.3.29:3000/'
+const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://137.184.3.29:3000'
 
 export const fetchProducts = async (term, filters = {}) => {
   let products = await axios.get(`${baseUrl}/v1/products/`, { params: {search: term, ...filters} })
